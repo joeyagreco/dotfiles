2331acd78c32ba950d061b6d27f2d2cbedbac24c
joeyagreco <joeyagreco@gmail.com>
2024-06-22 19:48:28 -0400

    better keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,18 @@ vim.g.mapleader = ' '
 
 -- toggle on / focus on explorer (<LEADER>e)
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeFocus<cr>', { noremap = true, silent = true })
--- close explorer (<LEADER>x)
-vim.api.nvim_set_keymap('n', '<leader>x', ':NvimTreeToggle<cr>', { noremap = true, silent = true })
+
 -- fuzzy find (<LEADER>ff)
 vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<cr>', { noremap = true, silent = true })
+
+-- TODO: find somewhere to put helper funcs
+function CloseNvimTreeIfOpen()
+	local view = require'nvim-tree.view'
+	if view.is_visible() then
+	  view.close()
+	end
+  end
+
+-- close explorer (<LEADER>x)
+vim.api.nvim_set_keymap('n', '<leader>x', [[:lua CloseNvimTreeIfOpen()<CR>]], { noremap = true, silent = true })
+
