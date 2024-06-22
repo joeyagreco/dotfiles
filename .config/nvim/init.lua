dcd3776c9c29f0c2717b5ea0348576466d56e893
joeyagreco <joeyagreco@gmail.com>
2024-06-22 13:19:26 -0400

    comment

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,6 +7,7 @@ dofile(vim.fn.stdpath('config') .. '/plugin/init.lua')
 -- Set leader to SPACE
 vim.g.mapleader = ' '
 
-
 -- Shortcuts
+
+-- open explorer (<LEADER>e)
 vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
