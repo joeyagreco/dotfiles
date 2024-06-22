bf7330cf74998a59a9c13d859cff7b43a49210ea
joeyagreco <joeyagreco@gmail.com>
2024-06-22 16:34:46 -0400

    cleanup

--- .config/nvim/plugin/init.lua
+++ .config/nvim/plugin/init.lua
@@ -1,54 +1,62 @@
 -- set up packer for plugin management
 require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
+	
 	-- nvim-tree
 	use 'nvim-tree/nvim-web-devicons'
 	use {
-		'nvim-tree/nvim-tree.lua',
-		requires = {
-			'nvim-tree/nvim-web-devicons', -- optional
-		 },
+	  'nvim-tree/nvim-tree.lua',
+	  requires = {
+		'nvim-tree/nvim-web-devicons',
+	  },
 	}
+  
+	-- telescope
 	use { 
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.8'
+	  'nvim-telescope/telescope.nvim',
+	  tag = '0.1.8',
+	  requires = { 'nvim-lua/plenary.nvim' }
 	}
-	-- required for telescope (1)
-	use 'nvim-lua/plenary.nvim'
-	-- required for telescope (2)
+	
+	-- treesitter
 	use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-        end,
-    }
-end)
-
-require('nvim-web-devicons').setup()
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive"
-  },
-  view = {
-    width = 50,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = false,
-  },
-})
-require('telescope').setup{
-  defaults = {
-    file_ignore_patterns = {},
-    hidden = true
+	  'nvim-treesitter/nvim-treesitter',
+	  run = function()
+		local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+		ts_update()
+	  end,
+	}
+  end)
+  
+  -- setup nvim-web-devicons
+  require('nvim-web-devicons').setup()
+  
+  -- setup nvim-tree
+  require("nvim-tree").setup({
+	sort = {
+	  sorter = "case_sensitive"
+	},
+	view = {
+	  width = 50,
+	},
+	renderer = {
+	  group_empty = true,
+	},
+	filters = {
+	  dotfiles = false,
+	},
+  })
+  
+  -- setup telescope
+  require('telescope').setup{
+	defaults = {
+	  file_ignore_patterns = {},
+	  hidden = true
+	}
   }
-}
-
-require('nvim-treesitter').setup()
-require'nvim-treesitter.configs'.setup {
+  
+  -- setup nvim-treesitter
+  require'nvim-treesitter.configs'.setup {
 	ensure_installed = { "lua", "python", "javascript" }, -- only list parsers that are needed
 	highlight = {
 	  enable = true,
