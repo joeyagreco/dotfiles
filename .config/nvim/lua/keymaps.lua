b7dc663396761b9663d69acc6c07363ec19d8e59
joeyagreco <joeyagreco@gmail.com>
2024-06-23 19:38:40 -0400

    more remap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,16 +1,14 @@
 -- import apis
 local telescope = require("telescope.builtin")
+local nvim_tree = require("nvim-tree.api")
 
 -- Set leader to SPACE
 vim.g.mapleader = " "
 
 -- toggle on / focus on explorer (<LEADER>e)
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>e",
-	":NvimTreeFocus<cr>",
-	{ noremap = true, silent = true, desc = "open / focus explorer" }
-)
+vim.keymap.set("n", "<leader>e", function()
+	nvim_tree.tree.toggle({ focus = true })
+end, { noremap = true, silent = true, desc = "open / focus explorer" })
 
 -- open a directory (<LEADER>o)
 -- TODO: need a better way to do this
@@ -31,6 +29,5 @@ vim.api.nvim_set_keymap(
 vim.keymap.set("n", "<leader>ff", function()
 	telescope.find_files({
 		follow = true, -- Follow symbolic links
-		--	hidden = true, -- Show hidden files
 	})
 end, { noremap = true, silent = true, desc = "find files" })
