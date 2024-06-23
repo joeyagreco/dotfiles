6b23f06d9138bd793555f19e6c1979ab33a31486
joeyagreco <joeyagreco@gmail.com>
2024-06-23 19:45:30 -0400

    cleanup keymaps

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -7,23 +7,19 @@ vim.g.mapleader = " "
 
 -- toggle on / focus on explorer (<LEADER>e)
 vim.keymap.set("n", "<leader>e", function()
-	nvim_tree.tree.toggle({ focus = true })
+	nvim_tree.tree.open({ focus = true })
 end, { noremap = true, silent = true, desc = "open / focus explorer" })
-
--- open a directory (<LEADER>o)
--- TODO: need a better way to do this
-vim.api.nvim_set_keymap("n", "<leader>o", ":NvimTreeToggle ", { noremap = true, silent = false, desc = "open a dir" })
 
 -- format code (<LEADER>fmt)
 vim.api.nvim_set_keymap("n", "<leader>fmt", ":Neoformat<cr>", { noremap = true, silent = true, desc = "format code" })
 
 -- search for word that cursor is on (<LEADER>fw)
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader>fw",
-	"<cmd>Telescope grep_string<CR>",
-	{ noremap = true, silent = true, desc = "search for word that cursor is on" }
-)
+vim.keymap.set("n", "<leader>fw", function()
+	telescope.grep_string({
+		word_match = "-w",
+		search = vim.fn.expand("<cword>"),
+	})
+end, { noremap = true, silent = true, desc = "search for word under cursor" })
 
 -- find files (<LEADER> ff)
 vim.keymap.set("n", "<leader>ff", function()
