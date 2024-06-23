6683c99d2090b088200f5eb0c09148f0c7772658
joeyagreco <joeyagreco@gmail.com>
2024-06-23 12:28:37 -0400

    stuff

--- .config/nvim/lua/plugin/init.lua
+++ .config/nvim/lua/plugin/init.lua
@@ -1,42 +1,40 @@
 -- set up packer for plugin management
 -- https://github.com/wbthomason/packer.nvim
-require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	
+require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
 	-- nvim-tree
-	use 'nvim-tree/nvim-web-devicons'
-	use {
-	  'nvim-tree/nvim-tree.lua',
-	  requires = {
-		'nvim-tree/nvim-web-devicons',
-	  },
-	}
-  
+	use("nvim-tree/nvim-web-devicons")
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	})
+
 	-- telescope
-	use { 
-	  'nvim-telescope/telescope.nvim',
-	  tag = '0.1.8',
-	  requires = { 'nvim-lua/plenary.nvim' }
-	}
-	
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+
 	-- treesitter
-	use {
-	  'nvim-treesitter/nvim-treesitter',
-	  run = function()
-		local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-		ts_update()
-	  end,
-	}
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
 	-- neoformat (format code)
 	-- https://github.com/sbdchd/neoformat
-	use 'sbdchd/neoformat' 
+	use("sbdchd/neoformat")
 	-- LSP servers
-	use 'neovim/nvim-lspconfig'
--- theme
-	use 'Abstract-IDE/Abstract-cs'
-        use 'marko-cerovac/material.nvim'
+	use("neovim/nvim-lspconfig")
+	-- theme
+	use("Abstract-IDE/Abstract-cs")
+	use("marko-cerovac/material.nvim")
 end)
-
 
 require("plugin/nvim_tree")
 require("plugin/nvim_web_devicons")
