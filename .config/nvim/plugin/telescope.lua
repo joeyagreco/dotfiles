2ae49ac3ea47d7292b7f4bbaf56a9c49f036f83a
joeyagreco-sb <joey.greco@superblockshq.com>
2024-06-24 10:26:33 -0400

    case insensitive live search

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -3,6 +3,15 @@ require("telescope").setup({
 	defaults = {
 		file_ignore_patterns = {},
 		hidden = true,
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+		},
 		mappings = {
 			i = {
 				["<esc>"] = actions.close,
