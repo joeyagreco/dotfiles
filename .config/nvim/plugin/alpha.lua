e731642354ce2b96f15dc981e03cb77485667c80
joeyagreco <joeyagreco@gmail.com>
2024-06-25 19:24:54 -0400

    more theming

--- .config/nvim/plugin/alpha.lua
+++ .config/nvim/plugin/alpha.lua
@@ -1,3 +1,5 @@
+-- custom theme discussion here: https://github.com/goolord/alpha-nvim/discussions/16
+
 local alpha = require("alpha")
 local alpha_themes = require("alpha.themes.startify")
 local dashboard = require("alpha.themes.dashboard")
@@ -21,4 +23,14 @@ dashboard.section.header.val = {
 	[[                                                                       ]],
 	[[                                                                       ]],
 }
+
+-- Set menu
+dashboard.section.buttons.val = {
+	dashboard.button("n", "   New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("f", "   Find file", ":Telescope find_files<CR>"),
+	dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+	dashboard.button("u", "󰂖   Update plugins", ":PackerSync<CR>"),
+	dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
+}
+
 alpha.setup(dashboard.opts)
