630674104f103c311f944275877539f16b6e63c2
joeyagreco-sb <joey.greco@superblockshq.com>
2024-06-26 09:52:43 -0400

    autocommand, compile plugins on plugin file change

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -8,6 +8,14 @@ vim.cmd([[
   augroup END
 ]])
 
+-- auto compile plugins when plugin file is modified
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost */plugin/*.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 -- use system clipboard
 vim.o.clipboard = "unnamedplus"
 
