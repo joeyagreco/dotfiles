bc56499cdb156fabf90277eb212c72a104477b41
joeyagreco-sb <joey.greco@superblockshq.com>
2024-06-26 09:37:42 -0400

    set cwd when changing git repo

--- .config/nvim/lua/helpers.lua
+++ .config/nvim/lua/helpers.lua
@@ -6,6 +6,7 @@ function M.prompt_and_open_git_repo()
 	local path = vim.fn.input("Path: ", constants.LOCAL_GIT_REPO_PATH .. "/")
 	vim.cmd("NvimTreeClose")
 	vim.cmd("NvimTreeOpen " .. vim.fn.fnameescape(path))
+	vim.cmd("cd " .. vim.fn.fnameescape(path))
 end
 
 return M
