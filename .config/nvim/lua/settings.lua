b5c17f8f37b3c987988248a1868a2afdea41929b
joeyagreco-sb <joey.greco@superblockshq.com>
2024-06-27 14:30:03 -0400

    disabled netrw and fix for closing alpha

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -23,11 +23,18 @@ vim.o.clipboard = "unnamedplus"
 vim.g.python3_host_prog = constants.PYTHON_PATH
 
 -- Auto-close Alpha buffer when opening a file
-vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = "*",
-	callback = function()
-		if vim.bo.filetype == "alpha" and #vim.fn.getbufinfo({ buflisted = 1 }) > 1 then
-			vim.cmd("bdelete")
-		end
-	end,
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = "*",
+  callback = function()
+    if vim.bo.filetype == "alpha" and #vim.fn.getbufinfo({ buflisted = 1 }) > 1 then
+      vim.cmd("bdelete")
+    end
+  end,
 })
+
+
+-- disable for nvim-tree
+-- :help nvim-tree-netrw
+-- https://github.com/nvim-tree/nvim-tree.lua?tab=readme-ov-file#install
+vim.g.loaded_netrw       = 1
+vim.g.loaded_netrwPlugin = 1
