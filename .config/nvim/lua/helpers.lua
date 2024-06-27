08821b2a68ea0a8446b1b6d7b9d54a9eb1f8cf03
joeyagreco <joeyagreco@gmail.com>
2024-06-26 23:23:02 -0400

    cleanup git helper func

--- .config/nvim/lua/helpers.lua
+++ .config/nvim/lua/helpers.lua
@@ -3,9 +3,12 @@ local constants = require("constants")
 local M = {}
 
 function M.prompt_and_open_git_repo()
-	local path = vim.fn.input("Path: ", constants.LOCAL_GIT_REPO_PATH .. "/")
-	vim.cmd("cd " .. vim.fn.fnameescape(path))
-	vim.cmd("NvimTreeOpen")
+	local local_git_path = constants.LOCAL_GIT_REPO_PATH .. "/"
+	local path = vim.fn.trim(vim.fn.input("Path: ", local_git_path))
+	if path ~= local_git_path and path ~= "" then
+		vim.cmd("cd " .. vim.fn.fnameescape(path))
+		vim.cmd("NvimTreeOpen")
+	end
 end
 
 function M.clean_and_update_plugins()
