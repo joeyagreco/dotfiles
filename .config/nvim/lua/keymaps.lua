37ed60bde3f772b9a4894914f31ef1d5cdbb37f1
joeyagreco <joeyagreco@gmail.com>
2024-07-06 00:42:43 -0400

    cleanup

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -34,18 +34,18 @@ local helpers = require("helpers")
 vim.g.mapleader = " "
 
 local default_options = { noremap = true, silent = true }
-local keyset = vim.keymap.set
+local map = vim.keymap.set
 
 -- toggle on / focus on explorer (<LEADER>e)
-keyset("n", "<leader>e", function()
+map("n", "<leader>e", function()
 	nvim_tree.tree.open({ focus = true })
 end, helpers.combine_tables(default_options, { desc = "open / focus explorer" }))
 
 -- format code (<LEADER>fm)
-keyset("n", "<leader>fm", ":Neoformat<cr>", helpers.combine_tables(default_options, { desc = "format code" }))
+map("n", "<leader>fm", ":Neoformat<cr>", helpers.combine_tables(default_options, { desc = "format code" }))
 
 -- search for word that cursor is on (<LEADER>fw)
-keyset("n", "<leader>fw", function()
+map("n", "<leader>fw", function()
 	telescope_builtin.grep_string({
 		word_match = "-w",
 		search = vim.fn.expand("<cword>"),
@@ -55,7 +55,7 @@ end, helpers.combine_tables(default_options, { desc = "search for word under cur
 
 -- search for a word (<LEADER>fs)
 -- https://github.com/BurntSushi/ripgrep/blob/master/GUIDE.md
-keyset(
+map(
 	"n",
 	"<leader>fs",
 	telescope.extensions.live_grep_args.live_grep_args,
@@ -63,7 +63,7 @@ keyset(
 )
 
 -- find files (<LEADER>ff)
-keyset("n", "<leader>ff", function()
+map("n", "<leader>ff", function()
 	telescope_builtin.find_files({
 		hidden = true,
 		file_ignore_patterns = { "node_modules", "build", "dist", "yarn.lock", ".git" },
@@ -71,7 +71,7 @@ keyset("n", "<leader>ff", function()
 end, helpers.combine_tables(default_options, { desc = "find files" }))
 
 -- go to definition for whatever the cursor is on (<LEADER>gd)
-keyset(
+map(
 	"n",
 	"<leader>gd",
 	vim.lsp.buf.definition,
@@ -79,7 +79,7 @@ keyset(
 )
 
 -- get lsp info for whatever the cursor is on (K)
-keyset(
+map(
 	"n",
 	"K",
 	vim.lsp.buf.hover,
@@ -87,7 +87,7 @@ keyset(
 )
 
 -- find references for whatever cursor is on
-keyset(
+map(
 	"n",
 	"<leader>fr",
 	telescope_builtin.lsp_references,
@@ -95,7 +95,7 @@ keyset(
 )
 
 -- find old (open up telescope search with previous search)
-keyset(
+map(
 	"n",
 	"<leader>fo",
 	telescope_builtin.resume,
@@ -103,7 +103,7 @@ keyset(
 )
 
 -- 'if __name__ == "__main__"' (<LEADER>inm)
-keyset(
+map(
 	"n",
 	"<leader>inm",
 	'iif __name__ == "__main__":<Esc>o',
@@ -111,13 +111,13 @@ keyset(
 )
 
 -- rename symbol
-keyset("n", "<leader>rn", vim.lsp.buf.rename, helpers.combine_tables(default_options, { desc = "rename symbol" }))
+map("n", "<leader>rn", vim.lsp.buf.rename, helpers.combine_tables(default_options, { desc = "rename symbol" }))
 
 -- toggle tree (<LEADER>t)
-keyset("n", "<leader>t", nvim_tree.tree.toggle, helpers.combine_tables(default_options, { desc = "toggle tree" }))
+map("n", "<leader>t", nvim_tree.tree.toggle, helpers.combine_tables(default_options, { desc = "toggle tree" }))
 
 -- turn search highlighting off (<LEADER>ho)
-keyset(
+map(
 	"n",
 	"<leader>ho",
 	":nohlsearch<CR>",
@@ -125,7 +125,7 @@ keyset(
 )
 
 -- open a git directory
-keyset(
+map(
 	"n",
 	"<leader>gi",
 	helpers.prompt_and_open_git_repo,
@@ -133,16 +133,16 @@ keyset(
 )
 
 -- quit vim (<LEADER>qq)
-keyset("n", "<leader>qq", ":qa<CR>", helpers.combine_tables(default_options, { desc = "exit vim" }))
+map("n", "<leader>qq", ":qa<CR>", helpers.combine_tables(default_options, { desc = "exit vim" }))
 
 -- copy url of current line in git
-keyset("n", "<leader>gu", function()
+map("n", "<leader>gu", function()
 	vim.cmd("GitBlameCopyFileURL")
 	print("git url copied")
 end, helpers.combine_tables(default_options, { desc = "copy url of current line in git" }))
 
 -- reset current cursor hunk
-keyset(
+map(
 	"n",
 	"<leader>gr",
 	git_signs.reset_hunk,
@@ -150,7 +150,7 @@ keyset(
 )
 
 -- reset all hunks in current file/buffer
-keyset(
+map(
 	"n",
 	"<leader>gR",
 	git_signs.reset_buffer,
@@ -158,7 +158,7 @@ keyset(
 )
 
 -- preview current cursor hunk
-keyset(
+map(
 	"n",
 	"<leader>gp",
 	git_signs.preview_hunk,
@@ -166,22 +166,17 @@ keyset(
 )
 
 -- toggle recent files
-keyset(
-	"n",
-	"<leader>R",
-	":Telescope oldfiles<CR>",
-	helpers.combine_tables(default_options, { desc = "see recent files" })
-)
+map("n", "<leader>R", ":Telescope oldfiles<CR>", helpers.combine_tables(default_options, { desc = "see recent files" }))
 
 -- comment out / uncomment line and selection (<LEADER>/)
-keyset(
+map(
 	"n",
 	"<leader>/",
 	comment.toggle.linewise.current,
 	helpers.combine_tables(default_options, { desc = "comment current line" })
 )
 
-keyset(
+map(
 	"v",
 	"<leader>/",
 	'<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>',
@@ -189,7 +184,7 @@ keyset(
 )
 
 -- clean and update plugins (<LEADER>pp)
-keyset(
+map(
 	"n",
 	"<leader>pp",
 	helpers.clean_and_update_plugins,
@@ -197,24 +192,24 @@ keyset(
 )
 
 -- open git diff view
-keyset("n", "<leader>dif", ":DiffviewOpen<CR>", helpers.combine_tables(default_options, { desc = "open git dif view" }))
+map("n", "<leader>dif", ":DiffviewOpen<CR>", helpers.combine_tables(default_options, { desc = "open git dif view" }))
 
 -- close tab
-keyset("n", "<leader>c", ":tabc<CR>", helpers.combine_tables(default_options, { desc = "close tab" }))
+map("n", "<leader>c", ":tabc<CR>", helpers.combine_tables(default_options, { desc = "close tab" }))
 
 -- go to last buffer
-keyset("n", "<leader>l", "<C-^>", helpers.combine_tables(default_options, { desc = "go to last buffer" }))
+map("n", "<leader>l", "<C-^>", helpers.combine_tables(default_options, { desc = "go to last buffer" }))
 
 -- auto import
-keyset("n", "<leader>i", function()
+map("n", "<leader>i", function()
 	vim.lsp.buf.code_action({ source = { organizeImports = true } })
 end, helpers.combine_tables(default_options, { desc = "go to last buffer" }))
 
 -- see lsp info
-keyset("n", "<leader>L", vim.diagnostic.open_float, helpers.combine_tables(default_options, { desc = "see lsp info" }))
+map("n", "<leader>L", vim.diagnostic.open_float, helpers.combine_tables(default_options, { desc = "see lsp info" }))
 
 -- focus from nvim tree -> main buffer
-keyset(
+map(
 	"n",
 	"<leader>m",
 	":wincmd l<CR>",
