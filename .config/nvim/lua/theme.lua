edb4439c73492fde25a155cdf79dbd2319268749
joeyagreco <joeyagreco@gmail.com>
2024-07-06 20:36:07 -0400

    revert theme

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,7 +1,7 @@
 -- supported themes: https://github.com/rockerBOO/awesome-neovim?tab=readme-ov-file#colorscheme
 
 -- https://github.com/Abstract-IDE/Abstract-cs
-vim.cmd("colorscheme abscs")
+-- vim.cmd("colorscheme abscs")
 -- https://github.com/marko-cerovac/material.nvim
--- vim.g.material_style = "deep ocean"
--- vim.cmd("colorscheme material")
+vim.g.material_style = "deep ocean"
+vim.cmd("colorscheme material")
