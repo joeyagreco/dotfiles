bb09ae132941dd12f39b569433bdc639fe8c6f18
joeyagreco-sb <joey.greco@superblockshq.com>
2024-07-11 09:48:19 -0400

    cleanup script

--- scripts/python/link_init.py
+++ scripts/python/link_init.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from util import print_color
 
 if __name__ == "__main__":
     IGNORE_THINGS = [
@@ -12,7 +13,7 @@ if __name__ == "__main__":
     ]
     local_git_repo_path = os.environ.get("LOCAL_GIT_REPO_PATH")
     if local_git_repo_path is None:
-        print("could not load local git repo path from $LOCAL_GIT_REPO_PATH")
+        print_color("could not load local git repo path from $LOCAL_GIT_REPO_PATH", color="red")
         exit(1)
 
     terminal_path = os.path.join(local_git_repo_path, "terminal")
@@ -21,7 +22,8 @@ if __name__ == "__main__":
     # links now holds the things we want to create a sym link for in $HOME
     home_dir_path = os.environ.get("HOME")
     if home_dir_path is None:
-        print("could not load home path from $HOME")
+        print_color("could not load home path from $HOME", color="red")
+        exit(1)
     home_dir_existing_links = os.listdir(home_dir_path)
     err_count = 0
     skipped_count = 0
@@ -39,14 +41,19 @@ if __name__ == "__main__":
             os.chdir(home_dir_path)
             result = subprocess.run(command, shell=True)
             if result.returncode != 0:
-                print(f"command failed with code {result.returncode}")
+                print_color(f"command failed with code {result.returncode}", color="red")
                 err_count += 1
             else:
-                print("success!")
+                print_color("success!", color="green")
         else:
             print(f"skipping creation of link '{link}' ...")
             skipped_count += 1
 
-    print(
+    color = "green"
+    if skipped_count > 0:
+        color = "yellow"
+    elif err_count > 0:
+        color = "red"
+    print_color(
         f"\nlinks creation completed with {err_count} errors and {skipped_count} skips"
-    )
+    , color=color)
