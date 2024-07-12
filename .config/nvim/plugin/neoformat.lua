ff28342dd39ac9096ea78617c8bc7750aeb3243b
joeyagreco-sb <joey.greco@superblockshq.com>
2024-07-12 16:05:29 -0400

    added clang format back

--- .config/nvim/plugin/neoformat.lua
+++ .config/nvim/plugin/neoformat.lua
@@ -1,15 +1,16 @@
 vim.g.neoformat_enabled_sh = { "shfmt" }
 vim.g.neoformat_enabled_zsh = { "shfmt" }
+vim.g.neoformat_enabled_go = { "gofmt" }
 
 -- proto format
 -- https://clang.llvm.org/docs/ClangFormat.html
 -- THIS ISNT WORKING RIGHT NOW WITH LINE LENGTH SET
--- vim.g.neoformat_enabled_proto = { "clangformat" }
--- vim.g.neoformat_cpp_clangformat = {
--- 	exe = "clang-format",
--- 	args = { "--style={ColumnLimit: 200}" },
--- 	stdin = true,
--- }
+vim.g.neoformat_enabled_proto = { "clang-format" }
+vim.g.neoformat_cpp_clangformat = {
+	exe = "clang-format",
+	args = { "--style={ColumnLimit: 200}" },
+	stdin = true,
+}
 
 -- Auto format on save using Neoformat
 vim.cmd([[
