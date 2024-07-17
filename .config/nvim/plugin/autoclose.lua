a4338b37f542dd3b57fbc2529c8191b12ff3004a
joeyagreco-sb <joey.greco@superblockshq.com>
2024-07-17 09:19:16 -0400

    include dollar sign in touch regex for autoclose

--- .config/nvim/plugin/autoclose.lua
+++ .config/nvim/plugin/autoclose.lua
@@ -1,8 +1,10 @@
+-- https://github.com/m4xshen/autoclose.nvim
 require("autoclose").setup({
 	options = {
 		disable_command_mode = true,
 		pair_spaces = true,
 		-- when next to a char, don't autoclose
 		disable_when_touch = true,
+		touch_regex = "[%w%(%[{%$]",
 	},
 })
