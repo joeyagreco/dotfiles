a11d8be460aa7dd0151ea3d122a085cee124cc50
joeyagreco-sb <joey.greco@superblockshq.com>
2024-07-19 10:37:01 -0400

    proto using buf-format

--- .config/nvim/plugin/neoformat.lua
+++ .config/nvim/plugin/neoformat.lua
@@ -1,22 +1,24 @@
 -- https://github.com/sbdchd/neoformat
 -- this is needed because by default, neoformat stops after a formatter succeeds
 vim.g.neoformat_run_all_formatters = 1
+
+-----------------------
+-- custom formatters --
+-----------------------
+-- proto
+vim.g.neoformat_proto_buf = {
+	exe = "buf",
+	args = { "format", "--write" },
+	replace = 1, -- Overwrite the file directly
+}
+
 vim.g.neoformat_enabled_sh = { "shfmt" }
 vim.g.neoformat_enabled_zsh = { "shfmt" }
 vim.g.neoformat_enabled_go = { "gofmt", "goimports" }
 vim.g.neoformat_enabled_yaml = { "prettier" }
 vim.g.neoformat_enabled_yml = { "prettier" }
 vim.g.neoformat_enabled_py = { "black", "isort" }
-
--- proto format
--- https://clang.llvm.org/docs/ClangFormat.html
--- THIS ISNT WORKING RIGHT NOW WITH LINE LENGTH SET
-vim.g.neoformat_enabled_proto = { "clang-format" }
-vim.g.neoformat_cpp_clangformat = {
-	exe = "clang-format",
-	args = { "--style={ColumnLimit: 200}" },
-	stdin = true,
-}
+vim.g.neoformat_enabled_proto = { "buf" }
 
 -- Auto format on save using Neoformat
 vim.cmd([[
