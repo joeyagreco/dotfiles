5f4b6d543e5ed347b24ceb891f8964a34d15feda
joeyagreco <joeyagreco@gmail.com>
2024-07-20 09:22:17 -0400

    javascript requied for treesitter

--- .config/nvim/plugin/treesitter.lua
+++ .config/nvim/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "go", "python", "typescript" },
+	ensure_installed = { "go", "python", "typescript", "javascript" },
 	highlight = {
 		enable = true,
 	},
