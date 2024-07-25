45b6f4b9fd3ae874ca5d06819e56ae415a251d43
joeyagreco <joeyagreco@gmail.com>
2024-07-24 22:41:46 -0400

    dont show preview for large files

--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -1,8 +1,28 @@
 local actions = require("telescope.actions")
 local telescope = require("telescope")
+local previewers = require("telescope.previewers")
+
+local handle_large_files = function(filepath, bufnr, opts)
+	-- size limit for previews (MB)
+	local max_file_size_mb = 1
+	local max_size = max_file_size_mb * 1024 * 1024
+	vim.loop.fs_stat(filepath, function(_, stat)
+		if not stat then
+			return
+		end
+		if stat.size > max_size then
+			-- skip showing preview
+			return
+		else
+			previewers.buffer_previewer_maker(filepath, bufnr, opts)
+		end
+	end)
+end
 
 telescope.setup({
 	defaults = {
+		-- don't show preview for large files
+		buffer_previewer_maker = handle_large_files,
 		path_display = { "smart" },
 		file_ignore_patterns = {
 			"build/",
