61ebb3f65e7ded845ef59478e8fb365200536740
joeyagreco <joeyagreco@gmail.com>
2024-07-26 13:47:44 -0500

    ruff for python config

--- .config/nvim/plugin/neoformat.lua
+++ .config/nvim/plugin/neoformat.lua
@@ -1,6 +1,8 @@
 -- https://github.com/sbdchd/neoformat
 -- this is needed because by default, neoformat stops after a formatter succeeds
 vim.g.neoformat_run_all_formatters = 1
+-- TURN ON FOR DEBUGGING ONLY
+vim.g.neoformat_verbose = 2
 
 -----------------------
 -- custom formatters --
@@ -12,12 +14,19 @@ vim.g.neoformat_proto_buf = {
 	replace = 1, -- Overwrite the file directly
 }
 
+vim.g.neoformat_python_ruff = {
+	exe = "ruff",
+	stdin = 1,
+	args = { "format", "-", "--config", vim.fn.expand("$HOME/ruff.toml") },
+	stderr = 1,
+}
+
 vim.g.neoformat_enabled_sh = { "shfmt" }
 vim.g.neoformat_enabled_zsh = { "shfmt" }
 vim.g.neoformat_enabled_go = { "gofmt", "goimports" }
 vim.g.neoformat_enabled_yaml = { "prettier" }
 vim.g.neoformat_enabled_yml = { "prettier" }
-vim.g.neoformat_enabled_py = { "black", "isort" }
+vim.g.neoformat_enabled_python = { "ruff" }
 vim.g.neoformat_enabled_proto = { "buf" }
 
 -- Auto format on save using Neoformat
--- scripts/python/link_init.py
+++ scripts/python/link_init.py
@@ -1,7 +1,9 @@
 import os
 import subprocess
 
-from util import print_color
+from util import (
+    print_color,
+)
 
 if __name__ == "__main__":
     # TODO: make this just use the .gitignore file
@@ -17,17 +19,24 @@ if __name__ == "__main__":
     local_git_repo_path = os.environ.get("LOCAL_GIT_REPO_PATH")
     if local_git_repo_path is None:
         print_color(
-            "could not load local git repo path from $LOCAL_GIT_REPO_PATH", color="red"
+            "could not load local git repo path from $LOCAL_GIT_REPO_PATH",
+            color="red",
         )
         exit(1)
 
-    terminal_path = os.path.join(local_git_repo_path, "terminal")
+    terminal_path = os.path.join(
+        local_git_repo_path,
+        "terminal",
+    )
     # filter out things we don't want to link
     links = [l for l in os.listdir(terminal_path) if l not in IGNORE_THINGS]
     # links now holds the things we want to create a sym link for in $HOME
     home_dir_path = os.environ.get("HOME")
     if home_dir_path is None:
-        print_color("could not load home path from $HOME", color="red")
+        print_color(
+            "could not load home path from $HOME",
+            color="red",
+        )
         exit(1)
     home_dir_existing_links = os.listdir(home_dir_path)
     err_count = 0
@@ -44,14 +53,21 @@ if __name__ == "__main__":
             print(f"creating link '{link}' ...")
             # ensure we are in the correct dir
             os.chdir(home_dir_path)
-            result = subprocess.run(command, shell=True)
+            result = subprocess.run(
+                command,
+                shell=True,
+            )
             if result.returncode != 0:
                 print_color(
-                    f"command failed with code {result.returncode}", color="red"
+                    f"command failed with code {result.returncode}",
+                    color="red",
                 )
                 err_count += 1
             else:
-                print_color("success!", color="green")
+                print_color(
+                    "success!",
+                    color="green",
+                )
         else:
             print(f"skipping creation of link '{link}' ...")
             skipped_count += 1
