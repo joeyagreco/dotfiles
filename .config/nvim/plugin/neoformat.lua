9da8eb9851762200b3d9b43d3c59535121b0fc6b
joeyagreco <joeyagreco@gmail.com>
2024-07-30 21:18:18 -0400

    python formatting

--- .config/nvim/plugin/neoformat.lua
+++ .config/nvim/plugin/neoformat.lua
@@ -14,10 +14,17 @@ vim.g.neoformat_proto_buf = {
 	replace = 1, -- Overwrite the file directly
 }
 
-vim.g.neoformat_python_ruff = {
+-- python
+vim.g.neoformat_python_ruff_check = {
 	exe = "ruff",
 	stdin = 1,
 	args = { "check", "-", "--config", vim.fn.expand("$HOME/ruff.toml"), "--fix", "-q" },
+	stderr = 1,
+}
+vim.g.neoformat_python_ruff_fmt = {
+	exe = "ruff",
+	stdin = 1,
+	args = { "format", "-", "--config", vim.fn.expand("$HOME/ruff.toml"), "-q" },
 	stderr = 1,
 }
 
@@ -34,7 +41,7 @@ vim.g.neoformat_enabled_zsh = { "shfmt" }
 vim.g.neoformat_enabled_go = { "gofmt", "goimports" }
 vim.g.neoformat_enabled_yaml = { "prettier" }
 vim.g.neoformat_enabled_yml = { "prettier" }
-vim.g.neoformat_enabled_python = { "ruff" }
+vim.g.neoformat_enabled_python = { "ruff_check", "ruff_fmt" }
 vim.g.neoformat_enabled_proto = { "buf" }
 vim.g.neoformat_enabled_typescript = {}
 
