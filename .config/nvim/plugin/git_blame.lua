10b186111e8e25a00aa2169de7c49d1f80383d04
joeyagreco <joeyagreco@gmail.com>
2024-08-07 19:04:54 -0400

    disable inline git blame, only use in lualine

--- .config/nvim/plugin/git_blame.lua
+++ .config/nvim/plugin/git_blame.lua
@@ -2,4 +2,7 @@
 require("gitblame").setup({
 	enabled = true,
 	date_format = "%b %d, %Y - %l:%M %p",
+	delay = 100, -- ms
+	message_when_not_committed = "Not committed",
+	display_virtual_text = false,
 })
