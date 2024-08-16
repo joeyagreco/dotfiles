d739ec330718d867d6632e09a838ba5141c77006
joeyagreco-sb <joey.greco@superblockshq.com>
2024-08-16 11:24:47 -0400

    comment

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -7,4 +7,5 @@ end, { desc = "Open LSP errors in quickfix list" })
 vim.api.nvim_create_user_command("Dif", "DiffviewOpen", { desc = "Open DiffView" })
 
 -- open merge conflicts in quickfix list
+-- mappings in plugin/git_conflict.lua
 vim.api.nvim_create_user_command("Con", "GitConflictListQf", { desc = "open merge conflicts in quickfix list" })
