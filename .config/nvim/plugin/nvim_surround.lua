5b2b867c79e0cab7a8d5b442644ba4b3bd6f1616
joeyagreco <joeyagreco@gmail.com>
2024-08-24 14:13:29 -0400

    better comment explanation for surround plugin

--- .config/nvim/plugin/nvim_surround.lua
+++ .config/nvim/plugin/nvim_surround.lua
@@ -2,4 +2,7 @@ local nvim_surround = require("nvim-surround")
 -- config: https://github.com/kylechui/nvim-surround/blob/main/lua/nvim-surround/config.lua
 nvim_surround.setup()
 
--- The three "core" operations of add/delete/change can be done with the keymaps ys{motion}{char}, ds{char}, and cs{target}{replacement}, respectively.
+-- add/delete/change:
+-- ys{motion}{char}
+-- ds{char}
+-- cs{target}{replacement}
