5eb1077bdd649d610512ae32f69bcc97b554f211
joeyagreco <joeyagreco@gmail.com>
2024-08-24 13:55:26 -0400

    plug in tmux bar and refresh at 10 instead of 15

--- scripts/bash/battery.sh
+++ scripts/bash/battery.sh
@@ -1,1 +1,8 @@
-pmset -g batt | grep -Eo "\d+%" | head -1
+battery_info=$(pmset -g batt)
+percentage=$(echo "$battery_info" | grep -Eo "\d+%" | head -1)
+
+if echo "$battery_info" | grep -q "AC Power"; then
+	echo " $percentage"
+else
+	echo "$percentage"
+fi
