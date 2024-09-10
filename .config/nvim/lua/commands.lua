ee9cd2e4a4dde1f0f9b6d966b9ba9f9c09dabdae
Joey Greco <joeyagreco@gmail.com>
2024-09-10 13:18:10 -0400

    pwd command

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -9,3 +9,10 @@ vim.api.nvim_create_user_command("Dif", "DiffviewOpen", { desc = "Open DiffView"
 -- open merge conflicts in quickfix list
 -- mappings in plugin/git_conflict.lua
 vim.api.nvim_create_user_command("Con", "GitConflictListQf", { desc = "open merge conflicts in quickfix list" })
+
+-- show and copy to clipboard the pwd of the current buffer
+vim.api.nvim_create_user_command("Pwd", function()
+	local pwd = vim.fn.expand("%:p:h")
+	vim.fn.setreg("+", pwd) -- Copy to system clipboard
+	print(pwd)
+end, { desc = "Show and copy to clipboard the pwd of the current buffer" })
