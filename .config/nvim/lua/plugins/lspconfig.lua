debb6f45f8633afe804c19d1968a2545d9a6f2cf
joeyagreco <joeyagreco@gmail.com>
2024-09-12 15:32:13 -0400

    buffers in lspconfig off

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,100 +1,100 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = { { "ms-jpq/coq_nvim", branch = "coq" }, { "ms-jpq/coq.artifacts", branch = "artifacts" } },
-	config = function()
-		local lspconfig = require("lspconfig")
+    "neovim/nvim-lspconfig",
+    dependencies = { { "ms-jpq/coq_nvim", branch = "coq" }, { "ms-jpq/coq.artifacts", branch = "artifacts" } },
+    config = function()
+        local lspconfig = require("lspconfig")
 
-		-- this must be before requiring coq for autostart to work: https://github.com/ms-jpq/coq_nvim/issues/403
-		-- true = autostart
-		-- shut-up = autostart and don't show startup message
-		vim.g.coq_settings = {
-			auto_start = "shut-up",
-			-- autocomplete was noisy and inaccurate
-			-- can tweak these until they seem nice
-			clients = {
-				-- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#lsp
-				lsp = {
-					enabled = true,
-				},
-				-- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#snippets
-				snippets = {
-					enabled = false,
-				},
-				-- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#treesitter
-				tree_sitter = {
-					enabled = true,
-				},
-				-- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#buffers
-				buffers = {
-					enabled = true,
-				},
-				-- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#paths
-				paths = {
-					enabled = true,
-				},
-				-- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#ctags
-				tags = {
-					enabled = false,
-				},
-			},
-		}
-		local coq = require("coq")
-		local coq_setup = coq.lsp_ensure_capabilities
+        -- this must be before requiring coq for autostart to work: https://github.com/ms-jpq/coq_nvim/issues/403
+        -- true = autostart
+        -- shut-up = autostart and don't show startup message
+        vim.g.coq_settings = {
+            auto_start = "shut-up",
+            -- autocomplete was noisy and inaccurate
+            -- can tweak these until they seem nice
+            clients = {
+                -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#lsp
+                lsp = {
+                    enabled = true,
+                },
+                -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#snippets
+                snippets = {
+                    enabled = false,
+                },
+                -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#treesitter
+                tree_sitter = {
+                    enabled = true,
+                },
+                -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#buffers
+                buffers = {
+                    enabled = false,
+                },
+                -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#paths
+                paths = {
+                    enabled = true,
+                },
+                -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#ctags
+                tags = {
+                    enabled = false,
+                },
+            },
+        }
+        local coq = require("coq")
+        local coq_setup = coq.lsp_ensure_capabilities
 
-		-- golang
-		lspconfig.gopls.setup(coq_setup({}))
+        -- golang
+        lspconfig.gopls.setup(coq_setup({}))
 
-		-- markdown
-		lspconfig.marksman.setup(coq_setup({}))
+        -- markdown
+        lspconfig.marksman.setup(coq_setup({}))
 
-		-- zsh
-		-- NOTE: use .shellcheckrc for configuration
-		lspconfig.bashls.setup(coq_setup({
-			filetypes = { "sh", "zsh", "zshrc" },
-		}))
+        -- zsh
+        -- NOTE: use .shellcheckrc for configuration
+        lspconfig.bashls.setup(coq_setup({
+            filetypes = { "sh", "zsh", "zshrc" },
+        }))
 
-		-- toml
-		lspconfig.taplo.setup(coq_setup({}))
+        -- toml
+        lspconfig.taplo.setup(coq_setup({}))
 
-		-- proto
-		lspconfig.protols.setup(coq_setup({}))
+        -- proto
+        lspconfig.protols.setup(coq_setup({}))
 
-		-- typescript
-		lspconfig.ts_ls.setup(coq_setup({}))
+        -- typescript
+        lspconfig.ts_ls.setup(coq_setup({}))
 
-		-- python
-		-- https://github.com/astral-sh/ruff-lsp
-		lspconfig.ruff_lsp.setup(coq_setup({}))
-		-- ruff lsp doesn't do "go to definition" well, so use pyright too
-		-- pyright config: https://github.com/microsoft/pyright/blob/main/docs/configuration.md
-		lspconfig.pyright.setup(coq_setup({
-			cmd = { "pyright-langserver", "--stdio" },
-		}))
+        -- python
+        -- https://github.com/astral-sh/ruff-lsp
+        lspconfig.ruff_lsp.setup(coq_setup({}))
+        -- ruff lsp doesn't do "go to definition" well, so use pyright too
+        -- pyright config: https://github.com/microsoft/pyright/blob/main/docs/configuration.md
+        lspconfig.pyright.setup(coq_setup({
+            cmd = { "pyright-langserver", "--stdio" },
+        }))
 
-		-- lua
-		lspconfig.lua_ls.setup({
-			settings = {
-				Lua = {
-					diagnostics = {
-						-- prevents "undefined global '<thing>'"
-						globals = { "vim", "ngx" },
-					},
-				},
-			},
-		})
+        -- lua
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        -- prevents "undefined global '<thing>'"
+                        globals = { "vim", "ngx" },
+                    },
+                },
+            },
+        })
 
-		-- yaml
-		lspconfig.yamlls.setup(coq_setup({
-			-- on_init = yaml_on_init,
-			filetypes = { "yaml", "yml", "yamlfmt" },
-		}))
+        -- yaml
+        lspconfig.yamlls.setup(coq_setup({
+            -- on_init = yaml_on_init,
+            filetypes = { "yaml", "yml", "yamlfmt" },
+        }))
 
-		-- makefile
-		lspconfig.efm.setup(coq_setup({
-			filetypes = { "make" },
-		}))
+        -- makefile
+        lspconfig.efm.setup(coq_setup({
+            filetypes = { "make" },
+        }))
 
-		-- vimrc
-		lspconfig.vimls.setup(coq_setup({}))
-	end,
+        -- vimrc
+        lspconfig.vimls.setup(coq_setup({}))
+    end,
 }
