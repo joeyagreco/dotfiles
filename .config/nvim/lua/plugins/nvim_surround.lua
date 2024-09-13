a683ce0a198fd8b484786aeceb2325848ed89454
Joey Greco <joeyagreco@gmail.com>
2024-09-13 14:21:30 -0400

    cleanup

--- .config/nvim/lua/plugins/nvim_surround.lua
+++ .config/nvim/lua/plugins/nvim_surround.lua
@@ -3,9 +3,8 @@ return {
     "kylechui/nvim-surround",
     lazy = false,
     config = function()
-        local nvim_surround = require("nvim-surround")
         -- config: https://github.com/kylechui/nvim-surround/blob/main/lua/nvim-surround/config.lua
-        nvim_surround.setup()
+        require("nvim-surround").setup()
 
         -- add/delete/change:
         -- ys{motion}{char}
