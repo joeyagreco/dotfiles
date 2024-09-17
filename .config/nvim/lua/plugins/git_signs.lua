086ed422d1efca8e5ff16e3e7a5803876bda420e
joeyagreco <joeyagreco@gmail.com>
2024-09-17 19:19:11 -0400

    config fix?

--- .config/nvim/lua/plugins/git_signs.lua
+++ .config/nvim/lua/plugins/git_signs.lua
@@ -2,9 +2,5 @@
 -- to refresh, run: ":Gitsigns refresh"
 return {
     "lewis6991/gitsigns.nvim",
-    lazy = false,
-    config = function()
-        -- trying this because it seems to not work with opts
-        require("gitsigns").setup()
-    end,
+    opts = {},
 }
