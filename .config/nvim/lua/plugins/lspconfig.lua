ddabc507eced38ebfa4c76beafe3d3845b353f1b
joeyagreco <joeyagreco@gmail.com>
2024-09-23 20:20:11 -0400

    switched from coq.nvim -> nvim-cmp

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -2,75 +2,75 @@
 -- if this is inside the config function, autocomplete does not work
 -- true = autostart
 -- shut-up = autostart and don't show startup message
-vim.g.coq_settings = {
-    auto_start = "shut-up",
-    -- autocomplete was noisy and inaccurate
-    -- can tweak these until they seem nice
-    clients = {
-        -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#lsp
-        lsp = {
-            enabled = true,
-        },
-        -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#snippets
-        snippets = {
-            enabled = false,
-        },
-        -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#treesitter
-        tree_sitter = {
-            enabled = false,
-        },
-        -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#buffers
-        buffers = {
-            enabled = false,
-        },
-        -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#paths
-        paths = {
-            enabled = true,
-        },
-        -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#ctags
-        tags = {
-            enabled = false,
-        },
-    },
-}
+-- vim.g.coq_settings = {
+--     auto_start = "shut-up",
+--     -- autocomplete was noisy and inaccurate
+--     -- can tweak these until they seem nice
+--     -- clients = {
+--     --     -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#lsp
+--     --     lsp = {
+--     --         enabled = true,
+--     --     },
+--     --     -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#snippets
+--     --     snippets = {
+--     --         enabled = false,
+--     --     },
+--     --     -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#treesitter
+--     --     tree_sitter = {
+--     --         enabled = false,
+--     --     },
+--     --     -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#buffers
+--     --     buffers = {
+--     --         enabled = false,
+--     --     },
+--     --     -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#paths
+--     --     paths = {
+--     --         enabled = true,
+--     --     },
+--     --     -- https://github.com/ms-jpq/coq_nvim?tab=readme-ov-file#ctags
+--     --     tags = {
+--     --         enabled = false,
+--     --     },
+--     -- },
+-- }
 return {
     "neovim/nvim-lspconfig",
     lazy = false,
-    dependencies = { { "ms-jpq/coq_nvim", branch = "coq" }, { "ms-jpq/coq.artifacts", branch = "artifacts" } },
+    -- dependencies = { { "ms-jpq/coq_nvim", branch = "coq" }, { "ms-jpq/coq.artifacts", branch = "artifacts" } },
     config = function()
         local lspconfig = require("lspconfig")
-        local coq = require("coq")
-        local coq_setup = coq.lsp_ensure_capabilities
+        -- local coq = require("coq")
+        -- local coq_setup = coq.lsp_ensure_capabilities
 
         -- golang
-        lspconfig.gopls.setup(coq_setup({}))
+        lspconfig.gopls.setup({})
 
         -- markdown
-        lspconfig.marksman.setup(coq_setup({}))
+        lspconfig.marksman.setup({})
 
         -- zsh
         -- NOTE: use .shellcheckrc for configuration
-        lspconfig.bashls.setup(coq_setup({
+        lspconfig.bashls.setup({
             filetypes = { "sh", "zsh", "zshrc" },
-        }))
+        })
 
         -- toml
-        lspconfig.taplo.setup(coq_setup({}))
+        lspconfig.taplo.setup({})
 
         -- proto
-        lspconfig.protols.setup(coq_setup({}))
+        lspconfig.protols.setup({})
 
         -- typescript
-        lspconfig.ts_ls.setup(coq_setup({}))
+        lspconfig.ts_ls.setup({})
 
         -- python
         -- https://github.com/astral-sh/ruff-lsp
-        lspconfig.ruff_lsp.setup(coq_setup({}))
+        lspconfig.ruff_lsp.setup({})
         -- ruff_lsp doesn't do "go to definition" at all (source: https://github.com/astral-sh/ruff-lsp/issues/57#issuecomment-1399540768), so use pyright too
-        lspconfig.pyright.setup(coq_setup({}))
+        lspconfig.pyright.setup({})
 
         -- lua
-        lspconfig.lua_ls.setup(coq_setup({
+        lspconfig.lua_ls.setup({
             settings = {
                 Lua = {
                     diagnostics = {
@@ -79,20 +79,20 @@ return {
                     },
                 },
             },
-        }))
+        })
 
         -- yaml
-        lspconfig.yamlls.setup(coq_setup({
+        lspconfig.yamlls.setup({
             -- on_init = yaml_on_init,
             filetypes = { "yaml", "yml", "yamlfmt" },
-        }))
+        })
 
         -- makefile
-        lspconfig.efm.setup(coq_setup({
+        lspconfig.efm.setup({
             filetypes = { "make" },
-        }))
+        })
 
         -- vimrc
-        lspconfig.vimls.setup(coq_setup({}))
+        lspconfig.vimls.setup({})
     end,
 }
---
+++ .config/nvim/lua/plugins/nvim_cmp.lua
@@ -0,0 +1,59 @@
+return {
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+    },
+    opts = function()
+        vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+        local cmp = require("cmp")
+        local defaults = require("cmp.config.default")()
+        local auto_select = true
+        return {
+            auto_brackets = {}, -- configure any filetype to auto add brackets
+            completion = {
+                completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+            },
+            preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+            mapping = cmp.mapping.preset.insert({
+                ["<TAB>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<S-TAB>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                ["<CR>"] = cmp.mapping.confirm({ select = auto_select }),
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "lspconfig" },
+                { name = "path" },
+            }, {
+                { name = "buffer" },
+            }),
+            formatting = {
+                format = function(entry, item)
+                    local widths = {
+                        -- width of the actual completion
+                        abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+                        -- width of extra info to the right of the completions
+                        menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 0,
+                    }
+
+                    for key, width in pairs(widths) do
+                        if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+                            item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+                        end
+                    end
+
+                    return item
+                end,
+            },
+            experimental = {
+                ghost_text = {
+                    hl_group = "CmpGhostText",
+                },
+            },
+            sorting = defaults.sorting,
+        }
+    end,
+}
