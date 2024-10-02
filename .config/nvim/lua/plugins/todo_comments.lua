6f748e3c6a1e1fbb07d860c4f4cb28a3732b2660
Joey Greco <joeyagreco@gmail.com>
2024-10-02 17:26:47 -0400

    lazy load comment plugin

--- .config/nvim/lua/plugins/todo_comments.lua
+++ .config/nvim/lua/plugins/todo_comments.lua
@@ -8,6 +8,8 @@ return {
     -- WARNING: foo
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = true,
+    event = "BufEnter",
     -- https://github.com/folke/todo-comments.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
     opts = {},
 }
