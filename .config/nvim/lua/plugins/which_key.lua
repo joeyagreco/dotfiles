ea49f41f6b423b98bad7e71d76719008853f10ea
Joey Greco <joeyagreco@gmail.com>
2024-10-08 10:06:43 -0400

    fix

--- .config/nvim/lua/plugins/which_key.lua
+++ .config/nvim/lua/plugins/which_key.lua
@@ -2,7 +2,10 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+        -- don't have some hints pop up when i start a key combo lmfao
+        triggers = {},
+    },
     keys = {
         {
             "<leader>W",
