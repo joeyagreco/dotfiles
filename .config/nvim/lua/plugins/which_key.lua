b3ca86b53c2a21f080ba1fc2eadc7ce9ed93d9ed
joeyagreco <joeyagreco@gmail.com>
2024-10-14 16:13:09 -0400

    whichkey plugin cleanup

--- .config/nvim/lua/plugins/which_key.lua
+++ .config/nvim/lua/plugins/which_key.lua
@@ -2,9 +2,19 @@
 return {
     "folke/which-key.nvim",
     event = "VeryLazy",
+    -- configuration: https://github.com/folke/which-key.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
     opts = {
-        -- don't have some hints pop up when i start a key combo lmfao
-        triggers = {},
+        triggers = {}, -- don't have some hints pop up when i start a key combo lmfao
+        plugins = {
+            marks = false, -- don't show a list of marks on ' and `
+            registers = false, -- don't show a list of registers on "
+            spelling = {
+                enabled = false,
+            },
+        },
+        win = {
+            no_overlap = false, -- allow the popup to overlap with the cursor
+        },
     },
     keys = {
         {
