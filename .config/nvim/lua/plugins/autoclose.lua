bede012b293680953982a24458b5e5e53bf76941
joeyagreco <joeyagreco@gmail.com>
2024-10-14 16:28:37 -0400

    disable ' in autoclose

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -4,12 +4,32 @@ return {
     lazy = true,
     event = "InsertEnter",
     opts = {
+        keys = {
+            ["("] = { escape = false, close = true, pair = "()" },
+            ["["] = { escape = false, close = true, pair = "[]" },
+            ["{"] = { escape = false, close = true, pair = "{}" },
+
+            [">"] = { escape = true, close = false, pair = "<>" },
+            [")"] = { escape = true, close = false, pair = "()" },
+            ["]"] = { escape = true, close = false, pair = "[]" },
+            ["}"] = { escape = true, close = false, pair = "{}" },
+
+            ['"'] = { escape = true, close = true, pair = '""' },
+            -- no clear workaround for making this work
+            -- EXAMPLE: > [try to type "didn't"]
+            --          > didn''t
+            -- for now just disable autoclose for ' by setting `close = false`
+            -- GITHUB ISSUE LINK: https://github.com/m4xshen/autoclose.nvim/issues/55
+            ["'"] = { escape = true, close = false, pair = "''" },
+            ["`"] = { escape = true, close = true, pair = "``" },
+        },
         options = {
-            disable_command_mode = true,
+            disabled_filetypes = { "text" },
+            disable_when_touch = false,
+            touch_regex = "[%w(%[{]",
             pair_spaces = true,
-            -- when next to a char, don't autoclose
-            disable_when_touch = true,
-            touch_regex = "[%w%(%[{%$]",
+            auto_indent = true,
+            disable_command_mode = true,
         },
     },
 }
