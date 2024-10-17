93b0168e0faae785f76710c0a36b56d1174c6c6f
Joey Greco <joeyagreco@gmail.com>
2024-10-17 12:33:16 -0400

    fixes for mise

--- zsh/sourcing.zsh
+++ zsh/sourcing.zsh
@@ -19,7 +19,7 @@ eval "$(~/.local/bin/mise activate zsh)"
 # PYTHON #
 ##########
 
-mise use -g python@$(cat .python-version) >/dev/null
+mise use -g python@$(cat ~/.python-version) >/dev/null
 # configure pyright
 export PYRIGHT_PYTHON_FORCE_VERSION='latest'
 
@@ -27,7 +27,7 @@ export PYRIGHT_PYTHON_FORCE_VERSION='latest'
 # JAVASCRIPT #
 ##############
 
-mise use -g node@$(cat .node-version) >/dev/null
+mise use -g node@$(cat ~/.node-version) >/dev/null
 # give node 2GB of memory
 export NODE_OPTIONS="--max-old-space-size=2000"
 
@@ -35,7 +35,7 @@ export NODE_OPTIONS="--max-old-space-size=2000"
 # GOLANG #
 ##########
 
-mise use -g go@$(cat .go-version) >/dev/null
+mise use -g go@$(cat ~/.go-version) >/dev/null
 export PATH="$HOME/go/bin:$PATH"
 export PATH="$GOROOT/bin:$PATH"
 
