89e1976fa77fb4acb718c6fe8cb03e0078a1ffe1
joeyagreco <joeyagreco@gmail.com>
2024-12-25 13:45:38 -0600

    disable when touch for autoclose

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -25,7 +25,7 @@ return {
         },
         options = {
             disabled_filetypes = { "text" },
-            disable_when_touch = false,
+            disable_when_touch = true,
             touch_regex = "[%w(%[{]",
             pair_spaces = true,
             auto_indent = true,
