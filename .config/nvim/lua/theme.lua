f7394e2df8a715c431d3b1807eda198767e2cb64
joeyagreco <joeyagreco@gmail.com>
2025-01-08 21:38:18 -0500

    stuff

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -4,3 +4,10 @@
 
 -- NIGHTFOX
 vim.cmd("colorscheme carbonfox")
+
+-----------
+-- edits --
+-----------
+
+-- make it easier to tell what is visually selected in vim
+vim.api.nvim_set_hl(0, "Visual", { reverse = true })
