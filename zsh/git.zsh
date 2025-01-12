659dbe820343e411116d0e6295d2f07526bed068
joeyagreco <joeyagreco@gmail.com>
2025-01-11 19:20:31 -0500

    global gitignore

---
+++ zsh/git.zsh
@@ -0,0 +1,3 @@
+# set a global gitignore file
+# to ensure this was set properly, run this: git config --get core.excludesfile
+git config --global core.excludesfile ~/.gitignore_global
--- zsh/init.zsh
+++ zsh/init.zsh
@@ -2,6 +2,7 @@ source "$ZSH_PATH/variables.zsh"
 source "$ZSH_PATH/sourcing.zsh"
 source "$ZSH_PATH/aliases.zsh"
 source "$ZSH_PATH/functions.zsh"
+source "$ZSH_PATH/git.zsh"
 # MUST STAY AT THE BOTTOM
 source "$HOME/.zshrc$LOCAL_FIX"
 
