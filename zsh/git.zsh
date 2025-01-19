46cec0b3b82d40bcfdeb552354228d231d7785c4
joeyagreco <joeyagreco@gmail.com>
2025-01-19 11:59:49 -0500

    comment

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,3 +1,3 @@
 # set a global gitignore file
-# to ensure this was set properly, run this: git config --get core.excludesfile
+# to check that this was set properly, run this: git config --get core.excludesfile
 git config --global core.excludesfile ~/.gitignore_global
