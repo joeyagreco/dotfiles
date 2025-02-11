9acc80ad7151103ba5d3889da3adbde47d02c224
joeyagreco <joeyagreco@gmail.com>
2025-02-11 16:12:31 -0500

    stuff

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -63,7 +63,14 @@ return {
         },
     },
     tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-live-grep-args.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+        },
+    },
     config = function()
         local actions = require("telescope.actions")
         local lga_actions = require("telescope-live-grep-args.actions")
@@ -147,10 +154,18 @@ return {
                         },
                     },
                 },
+                fzf = {
+                    fuzzy = true, -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true, -- override the file sorter
+                    case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                },
             },
         })
 
-        -- this must be last
+        -- these extensions MUST be loaded last
         telescope.load_extension("live_grep_args")
+        telescope.load_extension("fzf")
     end,
 }
