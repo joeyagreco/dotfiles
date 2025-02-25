8789b0af0d0be7be44602f5ba2362663c2a766fb
Joey Greco <joeyagreco@gmail.com>
2025-02-25 14:31:08 -0500

    better battery and cpu

--- scripts/bash/battery.sh
+++ scripts/bash/battery.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 battery_info=$(pmset -g batt)
-percentage=$(echo "$battery_info" | grep -Eo "\d+%" | head -1)
+percentage=$(echo "$battery_info" | grep -Eo "\d+%" | head -1 | tr -d '%')
 
 if echo "$battery_info" | grep -q "AC Power"; then
-	echo " $percentage"
+	echo " $percentage%"
 else
-	echo "$percentage"
+	if [ "$percentage" -ge 80 ]; then
+		icon=" "
+	elif [ "$percentage" -ge 60 ]; then
+		icon=" "
+	elif [ "$percentage" -ge 40 ]; then
+		icon=" "
+	elif [ "$percentage" -ge 20 ]; then
+		icon=" "
+	else
+		icon=" "
+	fi
+	echo "$icon $percentage%"
 fi
