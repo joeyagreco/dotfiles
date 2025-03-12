62a6f7546819249a27209c859e99bb1fe1c8a8f5
joeyagreco <joeyagreco@gmail.com>
2025-03-12 08:30:44 -0400

    quick commit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,10 +3,8 @@ require("vim")
 require("config.lazy")
 -- we do theme before anything else so in case we have an issue with config at least we can fix it while neovim looks nice
 require("theme")
--- TODO: use lazy autocommands: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 require("autocommands")
 require("constants")
 require("helpers")
 require("commands")
--- TODO: use lazy keymaps: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 require("keymaps")
