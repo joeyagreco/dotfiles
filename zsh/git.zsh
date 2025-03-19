49035950d7d340c0f66ed3b78e60e2a753d7a485
joeyagreco <joeyagreco@gmail.com>
2025-03-19 09:06:29 -0400

    gitconfig global

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,3 +1,7 @@
 # set a global gitignore file
 # to check that this was set properly, run this: git config --get core.excludesfile
 git config --global core.excludesfile ~/.gitignore_global
+
+# set a global gitconfig file
+# to check that this was set properly, run this: git config --get include.path
+git config --global include.path ~/.gitconfig_global
