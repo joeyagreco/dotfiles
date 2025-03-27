8a2e9936f845db366ed5ba816dfd09afc03b98bf
joeyagreco <joeyagreco@gmail.com>
2025-03-27 19:34:50 -0400

    feat(nvim): enable virtual text for diagnostics
    - Added configuration to enable virtual text for diagnostics in Neovim as of version 0.11.0. This enhances the visibility of diagnostic messages directly in the editor.

--- .config/nvim/lua/vim.lua
+++ .config/nvim/lua/vim.lua
@@ -97,3 +97,7 @@ vim.opt.gdefault = true
 
 -- don't show mode since our status line shows it instead
 vim.o.showmode = false
+
+-- this is opt-in as of nvim 0.11.0
+-- show diagnostics
+vim.diagnostic.config({ virtual_text = true })
