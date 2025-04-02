5d0082b4ef5716cf3b6a22a78afe0a710413db54
joeyagreco <joeyagreco@gmail.com>
2025-04-01 20:01:37 -0400

    no nvim web devicons

--- .config/nvim/lua/plugins/nvim_web_devicons.lua
+++
@@ -1,8 +0,0 @@
--- https://github.com/nvim-tree/nvim-web-devicons
-return {
-    "nvim-tree/nvim-web-devicons",
-    -- use mini.icons instead because they are faster
-    enabled = false,
-    lazy = true,
-    opts = {},
-}
