acf1e00c68526813e02ba91d99812bb2f77f1a9a
joeyagreco <joeyagreco@gmail.com>
2025-04-02 21:31:15 -0400

    Refactor: Remove Err command for opening LSP errors in quickfix list
    - Deleted the user command "Err" that opened all LSP errors in the quickfix list.
    - Kept the "Pwd" command for showing and copying the current buffer's directory.

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,8 +1,3 @@
--- Open all LSP errors in the current buffer in the quickfix list
-vim.api.nvim_create_user_command("Err", function()
-    vim.diagnostic.setloclist({ open = true })
-end, { desc = "open LSP errors in quickfix list" })
-
 -- show and copy to clipboard the pwd of the current buffer
 vim.api.nvim_create_user_command("Pwd", function()
     local pwd = vim.fn.expand("%:p:h")
