3dd5b1b3a826ff0d33c08d2f273956a802570b7b
joeyagreco <joeyagreco@gmail.com>
2025-04-03 20:16:29 -0400

    ``` fix: update nvim-ufo commit to c15fed422506a130a62e5e79c2d754392231d7d8
    Updated the lazy-lock.json to reflect the new commit hash for nvim-ufo, and removed the commented out version key for clarity.
    ```

--- .config/nvim/lua/plugins/nvim_ufo.lua
+++ .config/nvim/lua/plugins/nvim_ufo.lua
@@ -39,8 +39,6 @@ end
 return {
     "kevinhwang91/nvim-ufo",
     lazy = true,
-    -- version key isn't working so we pinning to the 1.4.0 commit hash for now
-    -- version = "1.4.0",
     commit = "c15fed422506a130a62e5e79c2d754392231d7d8",
     keys = {
         { "za", "za", desc = "toggle a fold", silent = true, noremap = true },
