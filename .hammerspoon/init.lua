5cc6af09ccc687c40b29a565c5670e143bc581b1
joeyagreco <joeyagreco@gmail.com>
2025-04-13 16:16:02 -0400

    quick commit

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,10 +1,19 @@
--- Map of hotkey -> application name
-local appHotkeys = {
-    J = "Google Chrome",
-    K = "Alacritty",
-}
+local json = require("hs.json")
+local hotkeyFile = os.getenv("HOME") .. "/.hammerspoon/hotkeys.json"
 
--- Bind each key to launch/focus the corresponding app
+local file = io.open(hotkeyFile, "r")
+if not file then
+    error("Could not open hotkeys.json")
+end
+
+local content = file:read("*a")
+file:close()
+
+local appHotkeys = json.decode(content)
+if not appHotkeys then
+    error("Failed to parse hotkeys.json")
+end
+
 for key, appName in pairs(appHotkeys) do
     hs.hotkey.bind({ "cmd", "shift" }, key, function()
         hs.application.launchOrFocus(appName)
