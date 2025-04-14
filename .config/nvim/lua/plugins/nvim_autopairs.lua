2b632a4b2b06ef29b15a77845c3eb8b5fefc98bd
Joey Greco <joeyagreco@gmail.com>
2025-04-14 11:07:15 -0400

    quick commit

--- .config/nvim/lua/plugins/nvim_autopairs.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/windwp/nvim-autopairs
-return {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-    opts = {},
-    -- disabling while trying out mini.pairs
-    enabled = false,
-}
