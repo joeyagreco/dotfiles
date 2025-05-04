d684847637b957dc090214b10edaf9172c614b8b
joeyagreco <joeyagreco@gmail.com>
2025-05-04 14:48:26 -0400

    more layers

--- .hammerspoon/layer.lua
+++ .hammerspoon/layer.lua
@@ -1,33 +1,29 @@
+-- to test keys in layers: https://en.key-test.ru/
 local menubar = hs.menubar.new()
 local alert = hs.alert
 
 local layerModules = {}
 local layerIndexByName = {}
-local currentIndex = 1 -- Lua tables are 1-indexed
+local currentIndex = 1 -- lua tables are 1-indexed
 
-local layerNames = {
-    "main",
-    "numbers",
+-- define layers and their activation hotkeys
+local layers = {
+    { name = "main", key = "M" },
+    { name = "numbers", key = "N" },
 }
-
--- Load and register layers
-for _, name in ipairs(layerNames) do
-    local ok, mod = pcall(require, "layers/" .. name)
-
-    if ok and mod and mod.enter and mod.exit then
-        table.insert(layerModules, mod)
-        layerIndexByName[name] = #layerModules
-    else
-        alert.show("Failed to load layer: " .. name)
-    end
-end
 
 local function showMenuBarLayer(layerName)
     menubar:setTitle(layerName .. "  ⌨️")
 end
 
+local function getCurrentLayerName()
+    return layerModules[currentIndex].name or ("Layer " .. currentIndex)
+end
+
+-- layer switching logic
 local function enterLayer(index)
     if index == currentIndex then
+        showMenuBarLayer(getCurrentLayerName())
         return
     end
 
@@ -37,38 +33,33 @@ local function enterLayer(index)
 
     currentIndex = index
 
-    local name = layerModules[currentIndex].name or ("Layer " .. currentIndex)
-    alert.show("Switched to: " .. name)
-    showMenuBarLayer(name)
+    showMenuBarLayer(getCurrentLayerName())
 
     if layerModules[currentIndex].enter then
         layerModules[currentIndex].enter()
     end
 end
 
--- Bind explicit layer switch keys
-hs.hotkey.bind({ "cmd", "shift" }, "N", function()
-    alert.show("number layer...")
-    local index = layerIndexByName["numbers"]
-    if index then
-        enterLayer(index)
-    end
-end)
+-- load and register layers
+for _, layer in ipairs(layers) do
+    local ok, mod = pcall(require, "layers/" .. layer.name)
 
-hs.hotkey.bind({ "cmd", "shift" }, "M", function()
-    alert.show("main layer...")
-    local index = layerIndexByName["main"]
-    if index then
-        enterLayer(index)
-    end
-end)
+    if ok and mod and mod.enter and mod.exit then
+        table.insert(layerModules, mod)
+        layerIndexByName[layer.name] = #layerModules
 
--- Start in default layer
-if layerIndexByName["main"] then
-    enterLayer(layerIndexByName["main"])
+        hs.hotkey.bind({ "cmd", "shift" }, layer.key, function()
+            enterLayer(layerIndexByName[layer.name])
+        end)
+    else
+        alert.show("failed to load layer: " .. layer.name)
+    end
 end
 
-alert.show("Total layers: " .. tostring(#layerModules))
+-- start in main layer
+enterLayer(layerIndexByName["main"])
+
+alert.show("loaded " .. tostring(#layerModules) .. " layers")
 
 return {
     enter = enterLayer,
