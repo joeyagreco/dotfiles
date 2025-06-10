9007cacd5b39de3d616963d32fb9a2fadc26d511
Joey Greco <joeyagreco@gmail.com>
2025-06-10 08:32:49 -0400

    blink cmp x mini icons

--- .config/nvim/lua/plugins/blink_cmp.lua
+++ .config/nvim/lua/plugins/blink_cmp.lua
@@ -2,8 +2,7 @@
 -- DOCS: https://cmp.saghen.dev/
 return {
     "saghen/blink.cmp",
-    -- optional: provides snippets for the snippet source
-    -- dependencies = { "rafamadriz/friendly-snippets" },
+    dependencies = { "echasnovski/mini.icons", "L3MON4D3/LuaSnip" },
     version = "1.*",
     opts = {
         keymap = {
@@ -19,8 +18,35 @@ return {
             nerd_font_variant = "mono",
         },
 
-        -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = false } },
+        completion = {
+            documentation = { auto_show = false },
+
+            menu = {
+                draw = {
+                    components = {
+                        kind_icon = {
+                            -- use icons from mini.icons
+                            text = function(ctx)
+                                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                                return kind_icon
+                            end,
+                            -- use highlights from mini.icons
+                            highlight = function(ctx)
+                                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                                return hl
+                            end,
+                        },
+                        kind = {
+                            -- use highlights from mini.icons
+                            highlight = function(ctx)
+                                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                                return hl
+                            end,
+                        },
+                    },
+                },
+            },
+        },
 
         -- Default list of enabled providers defined so that youggjkjkjk can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
