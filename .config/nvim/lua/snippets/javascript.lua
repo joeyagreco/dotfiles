38937b89de06b27093c34f8e0a5e9b70b42fb6d7
Joey Greco <joeyagreco@gmail.com>
2025-06-10 14:00:44 -0400

    snippet cleanup

--- .config/nvim/lua/snippets/javascript.lua
+++ .config/nvim/lua/snippets/javascript.lua
@@ -1,14 +1,11 @@
+local fmt = require("luasnip.extras.fmt").fmt
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
-local extras = require("luasnip.extras")
-local fmt = require("luasnip.extras.fmt").fmt
-local rep = extras.rep
-
 return {
-    s("cl", fmt('console.log("{}", {});', { i(1), rep(1) })),
+    s("cl", fmt('console.log("{}");', { i(1) })),
     s({
         trig = "afta",
         name = "afterAll block",
@@ -54,17 +51,6 @@ return {
         t({ "", "});" }),
     }),
     s({
-        trig = "cont",
-        name = "context block",
-        dscr = "Inserts a context block",
-    }, {
-        t("context('"),
-        i(1, ""),
-        t("', () => {"),
-        i(0),
-        t({ "", "});" }),
-    }),
-    s({
         trig = "desc",
         name = "describe block",
         dscr = "Inserts a describe block",
@@ -85,24 +71,5 @@ return {
         t("', async () => {"),
         i(0),
         t({ "", "});" }),
-    }),
-    --[[
-  ]]
-    --
-    s({
-        trig = "ajaxcomplete",
-        descr = "(ajaxcomplete)",
-        priority = -1000,
-        --trigEngine = te("w")
-    }, {
-        i(1, "obj", { key = "i1" }),
-        t(".ajaxComplete(function ("),
-        i(1, "e", { key = "i1" }),
-        t(", xhr, settings) {"),
-        --nl(),
-        t("\t"),
-        i(0, "// callback", { key = "i0" }),
-        --nl(),
-        t("});"),
     }),
 }
