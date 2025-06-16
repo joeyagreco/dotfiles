687e895f51bde81a1d79eff7216f651f26bd7446
joeyagreco <joeyagreco@gmail.com>
2025-06-16 07:59:18 -0400

    python snippets

--- .config/nvim/lua/snippets/python.lua
+++ .config/nvim/lua/snippets/python.lua
@@ -8,4 +8,8 @@ return {
         { trig = "inm", name = 'if __name__ == "__main__":' },
         { t('if __name__ == "__main__":'), t({ "", "    " }), i(0) }
     ),
+    s({ trig = "init", name = "__init__ function" }, { t("def __init__(self):"), t({ "", "    " }), i(0) }),
+    s({ trig = "str_", name = "__str__ function" }, { t("def __str__(self):"), t({ "", "    " }), i(0) }),
+    s({ trig = "repr_", name = "__repr__ function" }, { t("def __repr__(self):"), t({ "", "    " }), i(0) }),
+    s({ trig = "eq_", name = "__eq__ function" }, { t("def __eq__(self):"), t({ "", "    " }), i(0) }),
 }
