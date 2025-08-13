5e874a0ab0395ba8afe9b1b3509caf6c764511d8
joeyagreco <joeyagreco@gmail.com>
2025-08-12 22:49:49 -0400

    added ge command for dif view

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -4,8 +4,48 @@ return {
     "sindrets/diffview.nvim",
     lazy = true,
     cmd = { "Blame", "Dif" },
-    opts = {},
     config = function()
+        require("diffview").setup({
+            keymaps = {
+                file_panel = {
+                    -- this will open the current selected file in the :Dif file tree in a new buffer and close the dif view:w
+                    ["ge"] = function()
+                        local line = vim.api.nvim_get_current_line()
+                        local filename = line:match("^%s*[MAD?!]?%s*[^%s]*%s*([^%s]+)")
+
+                        if filename then
+                            -- get git root directory
+                            local handle = io.popen("git rev-parse --show-toplevel 2>/dev/null")
+                            local git_root = handle:read("*a"):gsub("\n", "")
+                            handle:close()
+
+                            -- try to find the file in the git repo
+                            local find_cmd = string.format("find %s -name %s -type f", git_root, filename)
+                            local find_handle = io.popen(find_cmd)
+                            local found_files = find_handle:read("*a")
+                            find_handle:close()
+
+                            local files = {}
+                            for file in found_files:gmatch("[^\r\n]+") do
+                                table.insert(files, file)
+                            end
+
+                            if #files > 0 then
+                                local file_to_open = files[1] -- use first match
+                                print("Opening: " .. file_to_open)
+                                vim.cmd("DiffviewClose")
+                                vim.schedule(function()
+                                    vim.cmd("edit " .. vim.fn.fnameescape(file_to_open))
+                                end)
+                            else
+                                print("Could not find file: " .. filename)
+                            end
+                        end
+                    end,
+                },
+            },
+        })
+
         -- open git dif view
         vim.api.nvim_create_user_command("Dif", ":lua require('diffview').open()<CR>", { desc = "Open DiffView" })
 
