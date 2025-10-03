65574a97e91b39a3bf666e4bd21a9005dfdf8da9
joeyagreco <joeyagreco@gmail.com>
2025-10-03 19:39:30 -0400

    quick commit

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -9,4 +9,23 @@ local helpers = require("helpers")
 
 -- NIGHTFOX
 -- https://github.com/EdenEast/nightfox.nvim
-return { "EdenEast/nightfox.nvim", lazy = false, priority = helpers.plugin_priority.THEME }
+return {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = helpers.plugin_priority.THEME,
+    config = function()
+        -- MATERIAL
+        -- vim.g.material_style = "deep ocean"
+        -- vim.cmd("colorscheme material")
+
+        -- NIGHTFOX
+        vim.cmd("colorscheme carbonfox")
+
+        -----------
+        -- edits --
+        -----------
+
+        -- make it easier to tell what is visually selected in vim
+        vim.api.nvim_set_hl(0, "Visual", { link = "IncSearch" })
+    end,
+}
