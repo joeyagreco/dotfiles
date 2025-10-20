a3691f2c9c3b0eb07cefd0bea33ac60dcb77742a
joeyagreco <joeyagreco@gmail.com>
2025-10-19 21:55:58 -0400

    resize window fixes

--- .config/hammerspoon/random.lua
+++ .config/hammerspoon/random.lua
@@ -1,34 +1,25 @@
-----------------------------------------------------------------------------------
--- make it so focused windows automatically go to fullscreen (but NOT maximize) --
-----------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------
+-- make it so focused windows automatically go to fullscreen (but NOT maximize)                          --
+-- NOTE: - ive tried a couple ways (with claude code) and none seem to work to resize windows properly   --
+-- NOTE: - this DOES cause the config to take several seconds to reload (instead of basically 0 seconds) --
+-----------------------------------------------------------------------------------------------------------
 
---NOTE: use application watcher instead of window.filter (avoids expensive window enumeration and long hammerspoon reload times)
-local appWatcher = nil
-if appWatcher then
-    appWatcher:stop()
-    appWatcher = nil
-end
+local windowFilter = hs.window.filter.new()
+windowFilter:subscribe(hs.window.filter.windowFocused, function(win)
+    if not win or not win:isStandard() then
+        return
+    end
 
-appWatcher = hs.application.watcher.new(function(_, eventType, _)
-    if eventType == hs.application.watcher.activated then
-        hs.timer.doAfter(0.01, function()
-            local win = hs.window.focusedWindow()
-            if win and win:isStandard() then
-                local frame = win:frame()
-                local screen = win:screen():frame()
-                local widthRatio = frame.w / screen.w
-                local heightRatio = frame.h / screen.h
+    local frame = win:frame()
+    local screen = win:screen():frame()
+    local widthRatio = frame.w / screen.w
+    local heightRatio = frame.h / screen.h
 
-                -- only maximize if window takes up at least 40% of screen in either dimension
-                -- NOTE: @joeyagreco - may have to tweak this number
-                if widthRatio > 0.4 or heightRatio > 0.4 then
-                    win:maximize()
-                end
-            end
-        end)
+    -- only maximize if window takes up at least 40% of screen in either dimension
+    if widthRatio > 0.4 or heightRatio > 0.4 then
+        win:maximize()
     end
 end)
-appWatcher:start()
 
 -------------------------------------
 -- don't want to see the dock icon --
