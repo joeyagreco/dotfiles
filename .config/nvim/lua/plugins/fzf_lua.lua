9017bc027d34532086cefb0617f2ec1485c6b51e
joeyagreco <joeyagreco@gmail.com>
2026-03-03 17:12:11 -0500

    quick commit

--- .config/nvim/lua/plugins/fzf_lua.lua
+++ .config/nvim/lua/plugins/fzf_lua.lua
@@ -8,6 +8,7 @@ return {
             "<leader>s",
             function()
                 require("fzf-lua").live_grep({
+                    fzf_opts = { ["--layout"] = "default" },
                     -- match telescope config: case insensitive, fixed strings, hidden files
                     rg_opts = "--column --line-number --no-heading --color=always --ignore-case --fixed-strings --hidden",
                     -- enable rg flags after "--" separator (e.g. "search term -- -tts")
