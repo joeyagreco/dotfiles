017935c8f01f2bb34069c71a0d70b8f121888325
joeyagreco <joeyagreco@gmail.com>
2026-04-19 13:57:01 -0400

    quick commit

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -20,8 +20,9 @@ hs.dockicon.hide()
 -----------------------------
 local end_time_ns = hs.timer.absoluteTime()
 local elapsed_sec = (end_time_ns - start_time_ns) / 1e9
+local show_message_duration_seconds = 1.5
 if elapsed_sec < 1 then
-    hs.alert.show(string.format("reloaded hammerspoon config in %.0f ms", elapsed_sec * 1000))
+    hs.alert.show(string.format("Hammerspoon reloaded (%.0f ms)", elapsed_sec * 1000), show_message_duration_seconds)
 else
-    hs.alert.show(string.format("reloaded hammerspoon config in %.1f seconds", elapsed_sec))
+    hs.alert.show(string.format("Hammerspoon reloaded (%.1f seconds)", elapsed_sec), show_message_duration_seconds)
 end
