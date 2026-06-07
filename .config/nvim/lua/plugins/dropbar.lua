4039bdebc551d80c65d35debe4c5aa0844d4ddd3
joeyagreco <joeyagreco@gmail.com>
2026-06-07 18:43:37 -0400

    make dropbar not show up for nvim tree buffer

--- .config/nvim/lua/plugins/dropbar.lua
+++ .config/nvim/lua/plugins/dropbar.lua
@@ -3,5 +3,20 @@ return {
     "Bekaboo/dropbar.nvim",
     lazy = true,
     event = "VeryLazy",
-    opts = {},
+    opts = function()
+        -- NOTE: @joeyagreco - we do all this extra crap just so this doesn't show up in certain places.. see the commit where this was added lol
+        -- wrap the default enable so we can keep all of dropbar's normal behavior
+        -- but never show the bar (which renders the "NvimTree_1" buffer name) in nvim-tree
+        local default_enable = require("dropbar.configs").opts.bar.enable
+        return {
+            bar = {
+                enable = function(buf, win, info)
+                    if vim.bo[buf].filetype == "NvimTree" then
+                        return false
+                    end
+                    return default_enable(buf, win, info)
+                end,
+            },
+        }
+    end,
 }
