fe100ac6c8356917ca88342c5dc695cfd06a3960
joeyagreco <joeyagreco@gmail.com>
2026-06-12 21:19:50 -0400

    more granular batter icon

--- scripts/bash/battery.sh
+++ scripts/bash/battery.sh
@@ -4,18 +4,30 @@ battery_info=$(pmset -g batt)
 percentage=$(echo "$battery_info" | grep -Eo "\d+%" | head -1 | tr -d '%')
 
 if echo "$battery_info" | grep -q "AC Power"; then
-	echo " $percentage%"
+	echo " $percentage%"
 else
-	if [ "$percentage" -ge 80 ]; then
-		icon=" "
+	if [ "$percentage" -ge 100 ]; then
+		icon="󰁹"
+	elif [ "$percentage" -ge 90 ]; then
+		icon="󰂂"
+	elif [ "$percentage" -ge 80 ]; then
+		icon="󰂁"
+	elif [ "$percentage" -ge 70 ]; then
+		icon="󰂀"
 	elif [ "$percentage" -ge 60 ]; then
-		icon=" "
+		icon="󰁿"
+	elif [ "$percentage" -ge 50 ]; then
+		icon="󰁾"
 	elif [ "$percentage" -ge 40 ]; then
-		icon=" "
+		icon="󰁽"
+	elif [ "$percentage" -ge 30 ]; then
+		icon="󰁼"
 	elif [ "$percentage" -ge 20 ]; then
-		icon=" "
+		icon="󰁻"
+	elif [ "$percentage" -ge 10 ]; then
+		icon="󰁺"
 	else
-		icon=" "
+		icon="󰂎"
 	fi
 	echo "$icon $percentage%"
 fi
